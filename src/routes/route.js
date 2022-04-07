e02b30b72e3843d8ac621e2d801df3e7ca73b42c
Amit <amit261199@gmail.com>
2022-04-07 22:06:15 +0530

    some changing

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,19 +3,23 @@ const logger = require('./logger')
 
 const router = express.Router();
 
-router.get('/user-profile/:abcd', function(req, res) {
-    console.log(req)
-    console.log(req.params.abcd)
-    res.send('dummy response')
-})
+// router.get('/user-profile/:abcd', function(req, res) {
+//     console.log(req)
+//     console.log(req.params.abcd)
+//     res.send('dummy response')
+// })
 
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
-});
+// router.get('/test-me', function (req, res) {
+//     console.log('------------------')
+//     console.log(req)
+//     console.log('------------------')
+//     console.log('These are the request query parameters: ', req.query)
+//     res.send('My first ever api!')
+// });
+
+router.get('/movies', function (req,res){
+    res.send(['Bahubali','Spiderman','Avengers','Wanted','Sholey'])
+})
 
 
 
