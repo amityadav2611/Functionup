4b844aa7ac051e0e05ee5e358ecccc40825e8c0a
Amit <amit261199@gmail.com>
2022-04-11 15:51:10 +0530

    mongo is written in index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://amit_261199:V3SMAdmMENRERSoU@cluster0.i9swe.mongodb.net/test",{
+    useNewUrlParser: true
+})
+.then( () => console.log("Mongodb is connected"))
+.catch( err => console.log(err))
 
 app.use('/', route);
 
