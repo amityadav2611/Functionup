0ce4089eeb9c6d7b2a09355a207ad21f077da515
Amit <amit261199@gmail.com>
2022-04-11 19:11:54 +0530

    some changes in index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 var bodyParser = require('body-parser');
 
 const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://amit_12345:YBJmgDvdTkfYXHY8@cluster0.hmujz.mongodb.net/test",{
+    useNewUrlParser: true
+})
+.then( () => console.log("Mongodb is connected"))
+.catch( err => console.log(err))
 
 app.use('/', route);
 
